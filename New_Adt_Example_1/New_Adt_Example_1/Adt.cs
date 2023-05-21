a2462ff6c427c925321a1d077397477ad6c62bc0
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-21 16:57:17 +0330

    Add files via upload

---
+++ New_Adt_Example_1/New_Adt_Example_1/Adt.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Adt_Example_1
+{
+    class Adt
+    {
+        public int[] dq;
+        public int rear1;
+        public int rear2;
+        public int max;
+        public int front1;
+        public int front2;
+
+        public Adt(int size)
+        {
+            dq = new int[size];
+            max = size;
+            rear1 = max / 2;
+            rear2 = (max / 2) - 1;
+            front1 = (max / 2) - 1;
+            front2 = max / 2;
+        }
+        public bool isfull()
+        {
+            if (rear1 == 0 && rear2 == (max - 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isempty(bool ch)
+        {
+            if (ch)
+            {
+                return (rear1 == max / 2);
+            }
+            else
+            {
+                return (rear2 == (max / 2) - 1);
+            }
+        }
+        public void insert(int data)
+        {
+            if (isfull())
+            {
+                Console.WriteLine("New Adt Is Full !");
+                return;
+            }
+            if (data >= 0)
+            {
+                dq[--rear1] = data;
+            }
+            else
+            {
+                dq[++rear2] = data;
+            }
+        }
+        public int remove(bool ch)
+        {
+            if (isempty(ch))
+            {
+                Console.WriteLine("Queue Is Empty !");
+                return -1;
+            }
+            int d;
+            if (ch)
+            {
+                d = dq[front1];
+                Console.WriteLine($"{d} Is Deleted...");
+                for (int i = front1; i >=rear1; i--)
+                {
+                    dq[i] = dq[i - 1];
+                }
+                rear1++;
+            }
+            else
+            {
+                d = dq[front2];
+                Console.WriteLine($"{d} Is Deleted...");
+                for (int i = front2; i <=rear2; i++)
+                {
+                    dq[i] = dq[i + 1];
+                }
+                rear2--;
+            }
+            return d;
+        }
+        public int length()
+        {
+            return ((rear1 + 1) + (max - rear2));
+        }
+        public void print()
+        {
+            for (int i = 0; i < max; i++)
+            {
+                if (dq[i] == 0)
+                {
+                    Console.Write(". ");
+                }
+                else
+                {
+                    Console.Write((dq[i] + " . "));
+                }
+            }
+        }
+        public void sort()
+        {
+
+        }
+        public void AddSort(int d)
+        {
+            if (isfull())
+            {
+                Console.WriteLine("Pore haji...!");
+                return;
+            }
+            else
+            {
+                if (d >= 0)
+                {
+                    //for
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ New_Adt_Example_1/New_Adt_Example_1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Adt_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adt q = new Adt(20);
+            q.insert(34);
+            q.insert(54);
+            q.insert(-23);
+            q.insert(99);
+            q.insert(59);
+            q.insert(-16);
+            q.insert(-19);
+            q.insert(-7);
+            q.insert(84);
+            q.insert(-90);
+            q.insert(-13);
+            q.insert(77);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter The Choice Remove ( R Or L ) : ");
+            string ch1 = Console.ReadLine();
+            if (ch1 == "l" || ch1 == "l")
+                q.remove(true);
+            else if (ch1 == "r" || ch1 == "R")
+                q.remove(false);
+            Console.WriteLine("\nNow Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Enter The Choice To Remove ( R Or L ) : ");
+            string ch2 = Console.ReadLine();
+            if (ch2 == "l" || ch2 == "L")
+                q.remove(true);
+            else if (ch2 == "r" || ch2 == "R")
+                q.remove(false);
+            Console.WriteLine("\nNow Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("The Length Of New Adt ( Double Queue Is ) : ");
+            int l = q.length();
+            Console.WriteLine(l);
+            Console.ResetColor();
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Please Enter A Number To Check & Insert : ");
+            int i = int.Parse(Console.ReadLine());
+            q.insert(i);
+            Console.WriteLine("\nNow Queue Is : ");
+            q.print();
+            Console.ResetColor();
+            Console.WriteLine("\n\n");
+
+            Console.ReadKey();
+        }
+    }
+}
